b793f6b9856a82898bff9b19473ef4b99842a555
MILADYS93 <duranmiladys@yahoo.es>
2023-09-12 17:42:18 -0500

    Se implementa diseño.

--- app/src/main/java/com/example/puebliandoapp/moldes/MoldeRestaurantes.java
+++ app/src/main/java/com/example/puebliandoapp/moldes/MoldeRestaurantes.java
@@ -9,7 +9,7 @@ public class MoldeRestaurantes implements Serializable {
     private String telefono;
     private String platoRecomendado;
     private Integer foto;
-
+    
     public MoldeRestaurantes() {
     }
 
